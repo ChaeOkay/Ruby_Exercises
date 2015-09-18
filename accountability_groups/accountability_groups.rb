8b805af183684a97bdb3afeaa7fdc491c59e0dc9
chaeokay <chae@bignerdranch.com>
2015-09-18 16:51:09 -0400

    Adds notes

--- accountability_groups/accountability_groups.rb
+++ accountability_groups/accountability_groups.rb
@@ -3,7 +3,7 @@ class Cohort
   attr_accessor :groups
 
   def initialize(roster, max: 5, min: 3)
-    @roster = roster
+    @roster = roster #[1,2,3,4,5,6,7,8,9,10,11]
     @groups = []
     @max = max
     @min = min
@@ -28,12 +28,21 @@ class Cohort
   end
 
   def combine_invalid_group
-    invalid_group = groups.pop
-    invalid_group << groups.pop
-    invalid_group.flatten
+    invalid_group = groups.pop #[11]
+    invalid_group << groups.pop #[6,7,8,9,10]
+    invalid_group.flatten #[11,6,7,8,9,10]
   end
 
   def divy(invalid_group, persons_per_group)
-    invalid_group.enum_for(:each_slice, persons_per_group).to_a
+    invalid_group.enum_for(:each_slice, persons_per_group).to_a #[[1,2,3,4,5],[6,7,8,9,10],[11]]
   end
 end
+
+
+# Not the best OOD, but gets the job done :)
+# If this code were to be used in a code base that would be around for awhile
+# and extensible, I would probably create:
+#   Cohort as collection of students
+#   Student as a Struct
+#   Group as a Class with a min and max size, holds a collection of student objects
+#   GroupGenerator that does the work
